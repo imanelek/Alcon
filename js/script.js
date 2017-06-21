5c26102c09303b8d620cce6597c6d2ced6d76b37
imanelek <imane.lekhzoumy@gmail.com>
2017-06-21 12:26:10 +0100

    1ere et 2eme partie d'integration de la landPage

---
+++ js/script.js
@@ -0,0 +1,18 @@
+ <script type="text/javascript">
+ alert("Please Fill All Required Field");
+    function validateForm()
+    {
+    var a=document.forms["Form"]["answer_a"].value;
+    var b=document.forms["Form"]["answer_b"].value;
+    var c=document.forms["Form"]["answer_c"].value;
+    var d=document.forms["Form"]["answer_d"].value;
+    var e=document.forms["Form"]["answer_e"].value;
+    var f=document.forms["Form"]["answer_f"].value;
+    var g=document.forms["Form"]["answer_g"].value;
+    if (a==null || a=="",b==null || b=="",c==null || c=="",d==null || d=="",e==null || e=="",f==null || f=="",g==null || g=="")
+      {
+      alert("Please Fill All Required Field");
+      return false;
+      }
+    }
+    </script>
