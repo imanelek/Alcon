2aae100c87bb9d0230306307d6aa88bef0aeecb6
imanelek <imane.lekhzoumy@gmail.com>
2017-06-23 13:34:08 +0100

    version mobile et desktop

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
  <script type="text/javascript">
- alert("Please Fill All Required Field");
-    function validateForm()
+    function verif()
     {
     var a=document.forms["Form"]["answer_a"].value;
     var b=document.forms["Form"]["answer_b"].value;
@@ -11,8 +10,9 @@
     var g=document.forms["Form"]["answer_g"].value;
     if (a==null || a=="",b==null || b=="",c==null || c=="",d==null || d=="",e==null || e=="",f==null || f=="",g==null || g=="")
       {
-      alert("Please Fill All Required Field");
+      alert("Merci de remplir les champs obligatoires");    
       return false;
       }
+    return true;
     }
-    </script>
+</script>
