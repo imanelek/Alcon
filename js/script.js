37e611ee8514389c7293f64d9562fe35bb9e0f20
imanelek <imane.lekhzoumy@gmail.com>
2017-06-29 12:46:46 +0100

    Changement de positionnement plus verification champs formulaire

--- js/script.js
+++ js/script.js
@@ -1,18 +1,68 @@
- <script type="text/javascript">
-    function verif()
-    {
-    var a=document.forms["Form"]["answer_a"].value;
-    var b=document.forms["Form"]["answer_b"].value;
-    var c=document.forms["Form"]["answer_c"].value;
-    var d=document.forms["Form"]["answer_d"].value;
-    var e=document.forms["Form"]["answer_e"].value;
-    var f=document.forms["Form"]["answer_f"].value;
-    var g=document.forms["Form"]["answer_g"].value;
-    if (a==null || a=="",b==null || b=="",c==null || c=="",d==null || d=="",e==null || e=="",f==null || f=="",g==null || g=="")
-      {
-      alert("Merci de remplir les champs obligatoires");    
-      return false;
-      }
-    return true;
-    }
+<script type="text/javascript"> 
+  
+function verif(){   
+ if(Form.answer_a.value == '' ) {   
+   alert('Merci de saisir votre nom');
+  Form.answer_a.focus(); 
+  return false; 
+ }   
+
+
+
+ if(Form.answer_b.value == '') {   
+  Form.answer_b.focus();   
+  return false;   
+ }   
+      
+
+ if(Form.answer_c.value == '' ) {   
+   alert('Merci de saisir votre nom');
+  Form.answer_c.focus(); 
+  return false; 
+ }   
+
+
+
+ if(Form.answer_d.value == '') {   
+  Form.answer_d.focus();   
+  return false;   
+ }   
+
+
+ if(Form.answer_e.value == '' ) {   
+   alert('Merci de saisir votre nom');
+  Form.answer_e.focus(); 
+  return false; 
+ }   
+
+
+
+ if(Form.answer_g.value == '') {   
+  Form.answer_g.focus();   
+  return false;   
+ }   
+
+
+
+
+ var c_postal = Form.answer_f.value;   
+
+     if ( c_postal == "" ){   
+  Form.answer_f.focus();   
+ return false;   
+ } else{  
+    var chiffres = new String(c_postal);    
+ chiffres = chiffres.replace(/[^0-9]/g, '');   
+ compteur = chiffres.length;   
+    
+ if (chiffres.length != 10){   
+  alert("Assurez-vous de rentrer un numéro à 10 chiffres (xxx-xxx-xxxx)");   
+  Form.answer_f.focus();   
+ return false;   
+ }   
+}  
+ return true;
+}   
+  
 </script>
+
